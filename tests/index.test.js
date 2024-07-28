9618342048d18cabcde334acf130a47d7be7fb80
shannenHere <shannenlauyensin@gmail.com>
2024-07-28 09:28:35 +0800

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,6 +2,6 @@
 const { app } = require('google-play-scraper');
 
 test('Test Google Play Scraper app function', async () => {
-  const appDetails = await app({ appId: 'com.example.app' });
+  const appDetails = await app({ appId: 'com.facebook.katana' });
   expect(appDetails).toHaveProperty('title');
 });
