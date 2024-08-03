f7309616d349c203f189013ebfe5e06e36837b75
shannenHere <shannenlauyensin@gmail.com>
2024-08-04 05:49:27 +0800

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,3 @@
-// jest.config.cjs
 module.exports = {
-  testMatch: ['**/tests/**'],
   testPathIgnorePatterns: ['/node_modules/'],
-  transform: {
-    '^.+\\.mjs$': 'babel-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
 };
